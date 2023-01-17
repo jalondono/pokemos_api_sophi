fec32964f4df2336610f569c330affed3868680b
lil-juan <juan.londono@southerncode.us>
2023-01-17 18:15:36 -0500

    [Update] ... - Added initial migrations - overriden the get_query set method on PokemonViewset, since is needed only show the public and owns pokemons

--- core/views/pokemon.py
+++ core/views/pokemon.py
@@ -2,9 +2,17 @@ from rest_framework import filters, mixins, pagination, status, viewsets
 from core.models.pokemon import Pokemon
 from core.serializers.pokemon import PokemonSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 
 
 class PokemonViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
+
+    def get_queryset(self):
+        """
+        Return the own pokemons and public
+        """
+        user = self.request.user
+        return Pokemon.objects.filter(Q(user_id=user.id) | Q(is_public=True))
